51bb881a028fb33f13562e042d09c8636bbebfa6
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-01-26 20:08:51 +0100

    Nice refactoring

--- src/components/challenges/04/chart.ts
+++ src/components/challenges/04/chart.ts
@@ -1,59 +1,145 @@
 import * as d3 from 'd3';
-export function lineChart(selection: d3.Selection<HTMLDivElement, any, any, any>) {
-    
+import { BottomCategoricalAxis, LeftLinearAxis } from 'ldd3';
+
+export class LineChart<T> {
+    private _group: d3.Selection<any, any, any, any>;
+    private _plotGroup: d3.Selection<any, any, any, any>;
+    private _xAxis: BottomCategoricalAxis<T>;
+    private _yAxis: LeftLinearAxis<T>;
+    private _lineGenerator: d3.Line<T>;
+    private _plotHeight: number;
+    private _x: (d: T) => string;
+    private _y: (d: T) => number;
+    private _groupBy: (d: T) => string;
+    constructor(selector: string, private _width: number, private _height: number) {
+        var width = _width;
+        var height = _height;
+        let svg = d3.select(selector)
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height);
+        this._group = svg;
+
+        let plotMargins = {
+            top: 60,
+            bottom: 30,
+            left: 60,
+            right: 90
+        };
+
+        svg.append('rect')
+            .attr('width', width)
+            .attr('height', height)
+            .style('fill', 'none')
+            .style('stroke', 'darkGray');
+        svg.append('g')
+            .classed('title', true)
+            .attr('transform', (d, i) => `translate(${width / 2},${30})`)
+            .append('text');
+
+        //TODO avoid nasty casting        
+        let plotGroup: d3.Selection<SVGGElement, T, any, any> = svg.append('g')
+            .classed('plot', true)
+            .attr('transform', `translate(${plotMargins.left},${plotMargins.top})`) as d3.Selection<SVGGElement, T, any, any>;
+
+        let plotWidth = width - plotMargins.left - plotMargins.right;
+        let plotHeight = height - plotMargins.top - plotMargins.bottom;
+        this._plotHeight = plotHeight;
+        this._plotGroup = plotGroup;
+        this._xAxis = new BottomCategoricalAxis(plotGroup, plotWidth, plotHeight);
+        this._yAxis = new LeftLinearAxis(plotGroup, plotWidth, plotHeight);
+        this._lineGenerator = d3.line<any>()
+            .curve(d3.curveStep)
+            .x(d => this._xAxis.scale(this._x(d)) + this._xAxis.bandWidth() / 2)
+            .y(d => this._yAxis.scale(parseInt(d['Regional Tourism Indicator (baseline 100)'])));
+    }
+
+    x(value: (d: T) => string): LineChart<T> {
+        if (arguments.length) {
+            this._x = value;
+        }
+        return this;
+    }
+
+    y(value: (d: T) => number): LineChart<T> {
+        if (arguments.length) {
+            this._y = value;
+        }
+        return this;
+    }
+
+    groupBy(value: (d: T) => string): LineChart<T> {
+        if (arguments.length) {
+            this._groupBy = value;
+        }
+        return this;
+    }
+
+    title(value: string): LineChart<T> {
+        this._group.select('.title')
+            .text(value);
+        return this;
+    }
+
+    update(data: Array<T>): void {
+        this._xAxis.domain(data.map(d => this._x(d)));
+        this._yAxis.domain([0, d3.max(data, d => this._y(d))]);
+
+        let grouped = d3.nest<T>()
+            .key(d => this._groupBy(d))
+            .entries(data);
+        var dataBound = this._plotGroup.selectAll('.year-series')
+            .data(grouped);
+        dataBound.exit()
+            .remove();
+        var enterSelection = dataBound
+            .enter()
+            .append('g')
+            .classed('year-series', true);
+        enterSelection.append('path')
+            .attr('d', (d: any) => this._lineGenerator(d.values))
+            .style('stroke', (d, i) => d3.schemeCategory10[i]);
+
+        // var legendWidth = 90;
+        // let legend = this._group.append('g')
+        //     .classed('legend', true)
+        //     .attr('transform', (d, i) => `translate(${this._width - legendWidth},${this._plotHeight / 2})`);
+        // let legendBound = legend.selectAll('.legend-item')
+        //     .data(d => d.byYear);
+        // legendBound.exit().remove();
+        // let enterLegend = legendBound.enter().append('g').classed('legend-item', true)
+        //     .attr('transform', (d, i) => `translate(${10},${(i + 1) * 20})`);
+        // enterLegend.append('rect')
+        //     .attr('width', 10)
+        //     .attr('height', 10)
+        //     .style('fill', (d, i) => d3.schemeCategory10[i]);
+        // enterLegend.append('text')
+        //     .attr('x', 15)
+        //     .attr('y', 9)
+        //     .text(d => d.key);
+    }
+}
+
+
+function lineChart(selection: d3.Selection<HTMLDivElement, any, any, any>) {
+
     let width = 800;
     let height = 400;
-    let svg = selection
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-
-    let plotMargins = {
-        top: 60,
-        bottom: 30,
-        left: 60,
-        right: 90
-    };
-    svg.append('rect')
-        .attr('width', width)
-        .attr('height', height)
-        .style('fill', 'none')
-        .style('stroke', 'darkGray');
-    svg.append('g')
-        .classed('title', true)
-        .attr('transform', (d, i) => `translate(${width / 2},${30})`)
-        .append('text')
-        .text(d => d.type);
-
-    let plotGroup = svg.append('g')
-        .classed('plot', true)
-        .attr('transform', `translate(${plotMargins.left},${plotMargins.top})`);
-
-    let plotWidth = width - plotMargins.left - plotMargins.right;
-    let plotHeight = height - plotMargins.top - plotMargins.bottom;
 
     let datum = selection.datum();
-    let xScale = d3.scaleBand<any>()
-        .domain(selection.datum().byYear[0].values.map((d: any) => d.Month))
-        .range([0, plotWidth]);
-    let xAxis = d3.axisBottom(xScale);
-    let xAxisGroup = plotGroup.append('g')
-        .classed('axis', true)
-        .attr('transform', (d, i) => `translate(${0},${plotHeight})`)
-        .call(xAxis);
 
-    let yScale = d3.scaleLinear()
-        .domain([0, 150])//d3.max(data, f => parseInt(f['Regional Tourism Indicator (baseline 100)']))])
-        // .nice()
-        .range([plotHeight, 0]);
-    let yAxis = d3.axisLeft(yScale);
-    let yAxisGroup = plotGroup.append('g')
-        .classed('axis', true)
-        .call(yAxis);
-    let lineGenerator = d3.line<any>()
-        .curve(d3.curveStep)
-        .x(d => xScale(d.Month)+xScale.bandwidth()/2)
-        .y(d => yScale(parseInt(d['Regional Tourism Indicator (baseline 100)'])));
+    // let yScale = d3.scaleLinear()
+    //     .domain([0, 150])//d3.max(data, f => parseInt(f['Regional Tourism Indicator (baseline 100)']))])
+    //     // .nice()
+    //     .range([plotHeight, 0]);
+    // let yAxis = d3.axisLeft(yScale);
+    // let yAxisGroup = plotGroup.append('g')
+    //     .classed('axis', true)
+    //     .call(yAxis);
+    // let lineGenerator = d3.line<any>()
+    //     .curve(d3.curveStep)
+    //     .x(d => xScale(d.Month) + xScale.bandwidth() / 2)
+    //     .y(d => yScale(parseInt(d['Regional Tourism Indicator (baseline 100)'])));
     var dataBound = plotGroup.selectAll('.year-series')
         .data(datum.byYear);
     dataBound
@@ -66,11 +152,11 @@ export function lineChart(selection: d3.Selection<HTMLDivElement, any, any, any>
     enterSelection.append('path')
         .attr('d', d => lineGenerator(d.values))
         .style('stroke', (d, i) => d3.schemeCategory10[i]);
-    
+
     var legendWidth = 90;
     let legend = svg.append('g')
         .classed('legend', true)
-        .attr('transform', (d, i) => `translate(${width-legendWidth},${plotHeight / 2})`);
+        .attr('transform', (d, i) => `translate(${width - legendWidth},${plotHeight / 2})`);
     let legendBound = legend.selectAll('.legend-item')
         .data(d => d.byYear);
     legendBound.exit().remove();
@@ -81,7 +167,8 @@ export function lineChart(selection: d3.Selection<HTMLDivElement, any, any, any>
         .attr('height', 10)
         .style('fill', (d, i) => d3.schemeCategory10[i]);
     enterLegend.append('text')
-    .attr('x', 15)    
-    .attr('y', 9)    
+        .attr('x', 15)
+        .attr('y', 9)
         .text(d => d.key);
+
 }
--- src/components/challenges/04/component.ts
+++ src/components/challenges/04/component.ts
@@ -1,7 +1,7 @@
 import * as d3 from 'd3';
 
 import { ICsvService } from '../../../services/csvService';
-import { lineChart } from './chart';
+import { LineChart } from './chart';
 export var mom04 = {
     name: 'mom04',
     component: {
@@ -25,13 +25,27 @@ function controller(csvService: ICsvService) {
         let map1 = byType.map(d => {
             return {
                 type: d.key,
-                byYear: d3.nest<any>().key(d => d.Year).entries(d.values)
+                byYear: d.values
             }
         });
-        d3.select('#chart')
-            .selectAll('svg')
-            .data(map1)
-            .enter()
-            .call(lineChart());
+        map1.forEach(m => {
+            try {
+                console.log(m);
+                var c = new LineChart<any>('#chart', 800, 400)
+                    .title(m.type)
+                    .x(d => d.Month)
+                    .y(d => parseInt(d['Regional Tourism Indicator (baseline 100)']))
+                    .groupBy(d => d.Year)
+                    .update(m.byYear);
+            } catch (e) {
+                console.error(e);
+            }
+
+        });
+        // d3.select('#chart')
+        //     .selectAll('svg')
+        //     .data(map1)
+        //     .enter()
+        //     .call(lineChart);
     };
 }
